58d2ab67e3045c69f666a07cd9c17b515fb9e577
Sam Fan <samisfan@msn.com>
2020-12-14 15:49:16 -0700

    started seed file, columns and fields complete, initial menu complete in js

--- employours.js
+++ employours.js
@@ -1,6 +1,6 @@
 // npm dependencies
 const mysql = require("mysql");
-const inquirer = require("inquier");
+const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 // connection
 const connection = mysql.createConnection({
@@ -13,6 +13,73 @@ const connection = mysql.createConnection({
 
 //functions
 // inquirer - department {id, name}, role {id, title, salary, department_id}, employee {id, first_name, last_name, role_id, manager_id}
+
+const menu = () => {
+  inquirer
+    .prompt({
+      type: "list",
+      name: "action",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "Exit",
+      ],
+    })
+    .then(({ action }) => {
+      switch (action) {
+        case "View All Employees":
+          console.log("Hey from All Employees");
+          break;
+        case "View All Employees By Department":
+          console.log("Hey from all employees by dept");
+          break;
+        case "View All Employees By Manager":
+          console.log("Hey from all employees by manager");
+          break;
+        case "Add Employee":
+          console.log("Hey From Add employee");
+          break;
+        case "Remove Employee":
+          console.log("Hey from remove employee");
+          break;
+        case "Update Employee Role":
+          console.log("Hey from update Employee Role");
+          break;
+        case "Update Employee Manager":
+          console.log("Hey from update employee Manager");
+          break;
+        case "Exit":
+          connection.end();
+      }
+    });
+};
+
+menu();
+
+// add department, add roles, add employees
+// view department, view roles, view employees, view employee by manager
+// update employee roles, update employee managers
+// Delete departments, roles, and employees
+// view (sort) total budget by dept, combine salaries
+
+// Constructor functions
+
+function View(category) {
+  console.log(`Selecting from ${category}..`);
+  connection.query(`SELECT * FROM ${category}`, (err, data) => {
+    if (err) throw err;
+    console.log(data);
+    connection.end;
+  });
+}
+
+const employees = new View(EMPLOYEE);
 
 connection.connect((err) => {
   if (err) throw err;
---
+++ seed.sql
@@ -0,0 +1,39 @@
+DROP DATABASE IF EXISTS employee_DB;
+
+CREATE DATABASE employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE employee (
+  employeeId INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    rolesId INTEGER,
+    managerId INTEGER
+    );
+
+CREATE TABLE department (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30)
+);
+
+CREATE TABLE roles (
+    id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL(6,0),
+    departId INTEGER
+);
+
+INSERT INTO department (dept_name) values ("Management");
+INSERT INTO department (dept_name) values ("Fieldwork");
+INSERT INTO department (dept_name) values ("Corporate");
+
+INSERT INTO roles (title, salary, departId) values ("Job Captain", 100000, 1);
+INSERT INTO roles (title, salary, departId) values ("Operator", 30000, 2);
+INSERT INTO roles (title, salary, departId) values ("Field_Coordinator", 500000, 3);
+
+INSERT INTO employee (first_name, last_name, rolesId, managerId) values ("V", "V", 3, 1);
+INSERT INTO employee (first_name, last_name, rolesId) values ("Dexter", "DeShawn", 1);
+INSERT INTO employee (first_name, last_name, rolesId, managerId) values ("Jackie", "Wells", 3, 1);
+
+SELECT * FROM employee;
