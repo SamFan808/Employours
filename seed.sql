e3c306f937cf458a3e0a2b1afdff058fd9199d59
Sam Fan <samisfan@msn.com>
2020-12-19 14:31:56 -0700

    added a few fields before recording demo

--- seed.sql
+++ seed.sql
@@ -12,7 +12,7 @@ CREATE TABLE department (
 CREATE TABLE roles (
     rId INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
-    salary DECIMAL(6,0),
+    salary DECIMAL(8,0),
     departId INTEGER,
     FOREIGN KEY(departId) REFERENCES department(dId)
 );
@@ -31,12 +31,14 @@ INSERT INTO department (dept_name) values ("Management");
 INSERT INTO department (dept_name) values ("Fieldwork");
 INSERT INTO department (dept_name) values ("Client Relations");
 INSERT INTO department (dept_name) values ("Technical Support");
+INSERT INTO department (dept_name) values ("Corporate");
 
 INSERT INTO roles (title, salary, departId) values ("Job Captain", 100000, 1);
 INSERT INTO roles (title, salary, departId) values ("Operator", 30000, 2);
 INSERT INTO roles (title, salary, departId) values ("Customer Service", 75000, 3);
 INSERT INTO roles (title, salary, departId) values ("Technician", 50000, 4);
 INSERT INTO roles (title, salary, departId) values ("Independant Contractor", 85000, 2);
+INSERT INTO roles (title, salary, departId) values ("CEO", 5000000, 4);
 
 INSERT INTO employee (first_name, last_name, rolesId) values ("Evelyn", "Parker", 3);
 INSERT INTO employee (first_name, last_name, rolesId) values ("Dexter", "DeShawn", 1);
@@ -44,6 +46,7 @@ INSERT INTO employee (first_name, last_name, rolesId, managerId) values ("Jackie
 INSERT INTO employee (first_name, rolesId, managerId) values ("V", 2, 2);
 INSERT INTO employee (first_name, last_name, rolesId) values ("Johnny", "Silverhand", 5);
 INSERT INTO employee (first_name, last_name, rolesId, managerId) values ("Judy", "Alvarez", 4, 1);
+INSERT INTO employee (first_name, last_name, rolesId) values ("Saburo", "Arasaka", 6);
 
 SELECT * FROM employee;
 SELECT * FROM roles;
